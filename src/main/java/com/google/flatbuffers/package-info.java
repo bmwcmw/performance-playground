2b815f4e0c9e9cd3b41d151c22e31bcd69022dca
CMWZ97I <syncpages@gmail.com>
2016-09-13 02:29:45 +0200

    Add pkg info

---
+++ src/main/java/com/google/flatbuffers/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * According to the official FlatBuffers project, FlatBuffers is a serialization
+ * library for games and other memory constrained apps.
+ * 
+ * FlatBuffers allows you to directly access serialized data without
+ * unpacking/parsing it first, while still having great forwards/backwards
+ * compatibility.
+ * 
+ * Unfortunately, FlatBuffers project does not publish any artifacts to the
+ * Maven Central Repository. In this fork, I keep a separate and intact copy of
+ * the FlatBuffers Java API for the purpose of publishing artifacts.
+ */
+package com.google.flatbuffers;
